eb7cbb911b87f4faa72fcd9f75084b86ba25b9da
Nitvex <vozisovnikita@gmail.com>
2018-10-10 19:10:52 +0300

    [Add] ajax-request to get products

--- src/client/components/Products/Products.js
+++ src/client/components/Products/Products.js
@@ -3,6 +3,22 @@ import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 
 class Products extends Component {
+
+    componentDidMount() {
+        let requestOptions = {
+            method: 'GET',
+            headers: new Headers(),
+            mode: 'cors',
+            cache: 'default'
+        };
+        fetch('http://127.0.0.1:3000/products', requestOptions).then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+        });
+
+    }
+
     render() {
         return (
             <div className="products">
