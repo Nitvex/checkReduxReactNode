6bd4c14589178571547df06b945394060de09f5e
Nitvex <vozisovnikita@gmail.com>
2018-10-10 19:38:30 +0300

    [Add] products showing

--- src/client/components/Products/Products.js
+++ src/client/components/Products/Products.js
@@ -4,6 +4,13 @@ import {Link} from "react-router-dom";
 
 class Products extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
     componentDidMount() {
         let requestOptions = {
             method: 'GET',
@@ -13,8 +20,9 @@ class Products extends Component {
         };
         fetch('http://127.0.0.1:3000/products', requestOptions).then((response) => {
             return response.json();
-        }).then((data) => {
-            console.log(data);
+        }).then((products) => {
+            console.log(products);
+            this.setState({products:products});
         });
 
     }
@@ -27,6 +35,11 @@ class Products extends Component {
                 <Link to="/">to Main</Link>
                 <br/>
                 <Link to="/numbers">Numbers</Link>
+                <ul>
+                    {this.state.products.map((p) => {
+                        return <li key={p.name}>{p.name} cost(s) {p.cost}</li>
+                    })}
+                </ul>
             </div>
         );
     }
